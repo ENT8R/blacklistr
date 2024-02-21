0a78ed99b12d448cd41d7f07a35036be5cdcba78
ENT8R <info.ent8r@gmail.com>
2024-02-21 10:38:12 +0100

    :coffee: Use instant-mocha instead of mochapack

--- webpack.config.test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mode: 'production',
-  target: 'node',
-  // Exclude modules that should not be bundled for testing
-  externals: /^(chai|fs)$/,
-  output: {
-    libraryTarget: 'commonjs'
-  }
-};
